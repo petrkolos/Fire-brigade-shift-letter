321a8c9b3efba0a3712805990ed70bfbd9008232
3Dpetr_kolos <59608345+petrkolos@users.noreply.github.com>
2024-11-13 22:19:37 +0100

    Create Cron.php

---
+++ Cron.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+//Import url WIth key - like = http://in.zivyobraz.eu/?import_key=SeCrEtApIkEy
+$importURL = 'http://in.zivyobraz.eu/?import_key=SeCrEtApIkEy';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
